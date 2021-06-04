7be40fabe603b382dfe70120aaa2f0d8231f1bc1
KSDragon82 <www.kiarashs2929@gmail.com>
2021-06-04 16:51:32 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const { hash } = window.location;
+const message = atob(hash.replace('#', ''));
+if (message) {
+	console.log(document.querySelector('#message-form'));
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#message-show').classList.remove('hide');
+	document.querySelector('h1').innerHTML = message;
+}
+document.querySelector('form').addEventListener('submit', event => {
+	event.preventDefault();
+	document.querySelector('#message-form').classList.toggle('hide');
+	document.querySelector('#link-form').classList.toggle('hide');
+	const input = document.querySelector('input');
+	const encrypted = btoa(input.value);
+	const linkInput = document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
+	linkInput.setSelectionRange(0, 99999);
+});
+
+const copy = () => {
+	const linkInput = document.querySelector('#link-input');
+	linkInput.select();
+	linkInput.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+};
+
+document.querySelector('#copy-btn').addEventListener('click', copy);
